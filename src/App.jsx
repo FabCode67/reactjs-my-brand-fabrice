0171e775dd636f4c1964715741b908d5bba41bb9
Fabrice MWANAFUNZI <mwanafunzifabrice@gmail.com>
2023-02-17 11:14:45 +0200

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Nav from '../src/components/nav/Nav'
+import Allblogs from '../src/components/allblogs/Allblogs'
+
 
 const App = () => {
     return (
-        <Nav/>
+        <>
+        <Nav />
+        <Allblogs />
+        </>
     )
 }
 
